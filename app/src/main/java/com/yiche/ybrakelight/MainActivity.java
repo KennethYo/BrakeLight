5a878f7d542c936c6f29df3ab911b6b20aab8c50
Kenneth <youzicong@gmail.com>
2016-11-07 10:41:00 +0800

    push to bintray

--- app/src/main/java/com/yiche/ybrakelight/MainActivity.java
+++ app/src/main/java/com/yiche/ybrakelight/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +16,20 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     TextView textView = (TextView) findViewById(R.id.text);
-    textView.setText(do_exec("dig www.baidu.com"));
+    textView.setText(do_exec("ping -R www.baidu.com"));
   }
 
   String do_exec(String cmd) {
     String s = "\n";
+    Process p = null;
+    BufferedReader in = null;
+    InputStream inputStream = null;
+    InputStreamReader inputStreamReader = null;
     try {
-      Process p = Runtime.getRuntime().exec(cmd);
-      BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      p = Runtime.getRuntime().exec(cmd);
+      inputStream = p.getInputStream();
+      inputStreamReader = new InputStreamReader(inputStream);
+      in = new BufferedReader(inputStreamReader);
       String line = null;
       while ((line = in.readLine()) != null) {
         s += line;
@@ -29,7 +37,23 @@ public class MainActivity extends AppCompatActivity {
     } catch (IOException e) {
       e.printStackTrace();
       MyApplication.getInstance().getWatch().watch(e);
+    } finally {
+      if (p != null) {
+        p.destroy();
+      }
+      close(in);
+      close(inputStream);
+      close(inputStreamReader);
     }
     return s;
+  }
+
+  private void close(Closeable close) {
+    if (close == null) return;
+    try {
+      close.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
