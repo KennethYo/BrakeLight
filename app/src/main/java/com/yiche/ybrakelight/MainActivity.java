a962650780753ec1d10c9558192b3ec569a1ef22
Kenneth <youzicong@gmail.com>
2016-11-07 11:52:14 +0800

    demo

--- app/src/main/java/com/yiche/ybrakelight/MainActivity.java
+++ app/src/main/java/com/yiche/ybrakelight/MainActivity.java
@@ -2,58 +2,28 @@ package com.yiche.ybrakelight;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import java.io.BufferedReader;
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    TextView textView = (TextView) findViewById(R.id.text);
-    textView.setText(do_exec("ping -R www.baidu.com"));
+    ButterKnife.bind(this);
   }
 
-  String do_exec(String cmd) {
-    String s = "\n";
-    Process p = null;
-    BufferedReader in = null;
-    InputStream inputStream = null;
-    InputStreamReader inputStreamReader = null;
+  @OnClick(R.id.btn) public void testCrash() {
+    throw new NullPointerException("test crash");
+  }
+
+  @OnClick(R.id.btn1) public void testCatch() {
+    String a = null;
     try {
-      p = Runtime.getRuntime().exec(cmd);
-      inputStream = p.getInputStream();
-      inputStreamReader = new InputStreamReader(inputStream);
-      in = new BufferedReader(inputStreamReader);
-      String line = null;
-      while ((line = in.readLine()) != null) {
-        s += line;
-      }
-    } catch (IOException e) {
+      char[] chars = a.toCharArray();
+    } catch (Exception e) {
       e.printStackTrace();
       MyApplication.getInstance().getWatch().watch(e);
-    } finally {
-      if (p != null) {
-        p.destroy();
-      }
-      close(in);
-      close(inputStream);
-      close(inputStreamReader);
-    }
-    return s;
-  }
-
-  private void close(Closeable close) {
-    if (close == null) return;
-    try {
-      close.close();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 }
--- app/src/main/java/com/yiche/ybrakelight/MyApplication.java
+++ app/src/main/java/com/yiche/ybrakelight/MyApplication.java
@@ -8,9 +8,10 @@ import com.yiche.library.brakelight.internal.BrakeLightWatch;
  * Created by kenneth on 2016/10/17.
  */
 
-public class MyApplication extends Application {
+public class MyApplication extends Application implements Thread.UncaughtExceptionHandler {
 
   private static MyApplication instance;
+  private Thread.UncaughtExceptionHandler exceptionHandler;
 
   public static MyApplication getInstance() {
     return instance;
@@ -22,9 +23,16 @@ public class MyApplication extends Application {
     super.onCreate();
     instance = this;
     watch = BrakeLight.install(this);
+    exceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+    Thread.setDefaultUncaughtExceptionHandler(this);
   }
 
   public BrakeLightWatch getWatch() {
     return watch;
+  }
+
+  @Override public void uncaughtException(Thread t, Throwable e) {
+    getWatch().watch(e);
+    exceptionHandler.uncaughtException(t, e);
   }
 }
